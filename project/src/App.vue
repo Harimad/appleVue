9b80c744072a47b4008a7b59a077ed3ab2c6ad59
Harimad <leesh68481@gmail.com>
2022-06-23 20:05:14 +0900

    Vue 이벤트 핸들러로 click 감지하기

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <p>{{ msg }}</p>
-  <!-- <p v-for="n in 10">{{ n }}</p> -->
-  <p v-for="상품 in products" :key="상품">{{ 상품 }}</p>
   <div class="menu">
-    <a v-for="(메뉴, i) in menus" :key="i">{{ i }}: {{ 메뉴 }}</a>
+    <a v-for="(메뉴, i) in menus" :key="i">{{ 메뉴 }}</a>
+  </div>
+  <div v-for="(상품, i) in products" :key="상품">
+    <p>{{ products[i] }}</p>
+    <button @click="increase(i)">허위매물신고</button>
+    <span> {{ 신고수[i] }}</span>
   </div>
 </template>
 
@@ -12,10 +14,15 @@ export default {
   name: 'App',
   data() {
     return {
-      msg: 'hello word',
+      신고수: [0, 1, 2],
       products: ['군자동원룸', ' 송정동원룸', '능동원룸'],
       menus: ['Home', 'About', 'Shop'],
     }
+  },
+  methods: {
+    increase(i) {
+      this.신고수[i]++
+    },
   },
 }
 </script>
@@ -27,12 +34,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .menu {
   background-color: aqua;
   padding: 20px;
+  border-radius: 20px;
 }
 .menu a {
   padding-right: 10px;
