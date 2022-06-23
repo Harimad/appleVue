57ed1fa4fe2f628efa26176b60fc0244e7e516a5
Harimad <leesh68481@gmail.com>
2022-06-23 20:47:09 +0900

    v-if 와 모달창 만들기 (Vue에서 동적인 UI 만드는 법)

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,19 +1,34 @@
 <template>
+  <!-- 모달창 -->
+  <div class="black-bg" v-if="모달창상태">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="modalToggle">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(메뉴, i) in menus" :key="i">{{ 메뉴 }}</a>
   </div>
   <div v-for="(상품, i) in products" :key="상품">
-    <p>{{ products[i] }}</p>
+    <img :src="방이미지[i]" class="roomImg" />
+    <p @click="modalToggle">{{ products[i] }}</p>
     <button @click="increase(i)">허위매물신고</button>
     <span> {{ 신고수[i] }}</span>
   </div>
 </template>
-
 <script>
 export default {
   name: 'App',
   data() {
     return {
+      모달창상태: false,
+      방이미지: [
+        require('./assets/images/room0.jpg'),
+        require('./assets/images/room1.jpg'),
+        require('./assets/images/room2.jpg'),
+      ],
       신고수: [0, 1, 2],
       products: ['군자동원룸', ' 송정동원룸', '능동원룸'],
       menus: ['Home', 'About', 'Shop'],
@@ -23,17 +38,33 @@ export default {
     increase(i) {
       this.신고수[i]++
     },
+    modalToggle() {
+      if (this.모달창상태) this.모달창상태 = !this.모달창상태
+      else this.모달창상태 = !this.모달창상태
+    },
   },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
   text-align: center;
-  color: #2c3e50;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 
 .menu {
@@ -43,5 +74,10 @@ export default {
 }
 .menu a {
   padding-right: 10px;
+}
+
+.roomImg {
+  width: 80%;
+  padding: 20px;
 }
 </style>
